be1e2e141495c14560fdb7de249b8e8325ef6cce
Borun Kumer Mondal <86507432+B-K-Pronoy@users.noreply.github.com>
2022-02-18 19:38:23 +0600

    first commit

---
+++ 5update/app.js
@@ -0,0 +1,75 @@
+
+
+  function alertFantionForIncomeEmty(){
+    const alertCatch=document.getElementById('forIncomEmpty');
+    alertCatch.style.display='Block'
+  }
+  function alertFantionForNagative(){
+    const alertCatch=document.getElementById('ifIncomNegative');
+    alertCatch.style.display='Block';
+  };
+
+
+/* for food */
+
+  function alertFantionForFoodEmty(){
+    const alertCatch=document.getElementById('forfoodEmpty');
+    alertCatch.style.display='Block'; 
+  }
+  function alertFantionForNagative(){
+    const alertCatch=document.getElementById('ifIncomNegative');
+    alertCatch.style.display='Block';
+  }
+
+/*  for rent */
+
+function alertFantionForRentEmty(){
+    const alertCatch=document.getElementById('forRentEmpty');
+    alertCatch.style.display='Block'
+  }
+
+
+/*  for clothes */
+
+function alertFantionForClothesEmty(){
+    const alertCatch=document.getElementById('forClothEmpty');
+    alertCatch.style.display='Block'
+  }
+
+
+/*  for total expense */
+
+function alertFantionForTotalExpnse(){
+    const alertCatch=document.getElementById('forTotalExpense');
+    alertCatch.style.display='Block'
+
+   
+  }
+
+  function alertFantionForSaveTotalTaka()
+
+  {
+    const alertCatch=document.getElementById('saveBlanceErrow');
+    alertCatch.style.display='Block'
+  }
+
+
+function alertFantion(){
+    const alertCatch=document.getElementById('container');
+    alertCatch.style.display='Block'
+  }
+  
+
+
+
+
+  function alertFantionForNotEnougthMoney(){
+    const alertCatch=document.getElementById('notEnoughtMoney');
+    alertCatch.style.display='Block'
+  }
+
+
+  function alertFantionForBlance(){
+    const alertCatch=document.getElementById('forBlance');
+    alertCatch.style.display='Block'
+  }
---
+++ 5update/costom.js
@@ -0,0 +1,118 @@
+const ClaclucateBtn=document.getElementById('Claclucate').addEventListener('click',function(e){
+
+    // for income
+    const inputeIncome=document.getElementById('incomeAmaunt');
+    const inputeTextOfIncome=inputeIncome.value;
+    if (inputeTextOfIncome=== '' || inputeTextOfIncome == null){
+      alertFantionForIncomeEmty();
+    
+    }
+   else if(inputeTextOfIncome<0){
+    alertFantionForNagative();
+   
+
+   }
+    const inputeAmaunt=parseInt(inputeTextOfIncome);
+
+        
+    // for food
+    const inputeFood=document.getElementById('fod');
+    const inputeTextOfFood=inputeFood.value;
+    if (inputeTextOfFood=== '' ||inputeTextOfFood == null){
+      alertFantionForFoodEmty();
+    
+    }
+    else if(inputeTextOfFood<0){
+      alertFantionForNagative();
+  
+     }
+
+
+    const foodAmount=parseInt(inputeTextOfFood);
+
+    //for rent
+    const inputeRent=document.getElementById('rent');
+    const inputeTextOfRent=inputeRent.value;
+    
+    if (inputeTextOfRent=== '' ||inputeTextOfRent == null){
+      alertFantionForRentEmty();
+    
+    }
+    else if(inputeTextOfRent<0){
+      alertFantionForNagative();
+  
+     }     
+
+    const rentAmount=parseInt(inputeTextOfRent);
+    //for clothes
+    const inputeClothes=document.getElementById('clothes');
+    const inputeTextOfClothes=inputeClothes.value;
+    if (inputeTextOfClothes=== '' ||inputeTextOfClothes == null){
+      alertFantionForClothesEmty();
+    
+    }
+    else if(inputeTextOfClothes<0){
+      alertFantionForNagative();
+  
+     }
+
+    
+    const colthesAmount=parseInt(inputeTextOfClothes);
+    //for totla expense
+
+    const totalExpense=foodAmount+rentAmount+colthesAmount;
+    if(totalExpense>inputeAmaunt){
+      alertFantionForBlance();
+    }
+    else if(totalExpense<0){
+      alertFantionForTotalExpnse();
+    }
+   
+
+    const totalExpenseShow=document.getElementById('totalExpen');
+   totalExpenseShow.innerText=totalExpense;
+
+   const totalBlance=document.getElementById('totalBlance');
+       const totalBlanceAmaunt=inputeAmaunt-totalExpense;
+        const blance=totalBlanceAmaunt;
+        totalBlance.innerText=blance;
+    
+       
+
+});
+ // for save
+const  saveBtnCatch=document.getElementById('saveBtn').addEventListener('click',function(){
+
+  const incomeAmaunt=document.getElementById('incomeAmaunt');
+  const incomeTaka= parseInt(incomeAmaunt.value);
+ 
+
+      const saveParsent=document.getElementById('saveInpute');
+      const saveAmaunt=parseInt(saveParsent.value)
+      if (0>=saveAmaunt){
+       
+      }
+
+      
+      const saveTotalTk=(incomeTaka*saveAmaunt)/100;
+    
+      const totalSave=document.getElementById('saveAmount');
+        totalSave.innerText=saveTotalTk;
+        const remainingAmaunt=document.getElementById('remainingBlance');
+
+        const priveBlance=document.getElementById('totalBlance');
+      const privesBlanceTaka=parseInt(priveBlance.innerText);
+      if(saveTotalTk>privesBlanceTaka){
+        alertFantionForSaveTotalTaka();
+      }
+      if(saveTotalTk>privesBlanceTaka){
+        remainingAmaunt.innerText='No enfoe for remaing';
+      }
+      else{
+        remainingAmaunt.innerText=privesBlanceTaka-saveTotalTk;
+
+      }
+        
+    });
+    
+   
